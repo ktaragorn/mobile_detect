f4b14cc1b535a16a717427635cf9b159e543d6ed
Sebastian Wallin <sebastian.wallin@gmail.com>
2016-03-05 17:14:14 -0800

    Apply JSON formatting when fetching rules file

--- data/get_latest_data.sh
+++ data/get_latest_data.sh
@@ -1,1 +1,1 @@
-wget -N https://raw.githubusercontent.com/serbanghita/Mobile-Detect/master/Mobile_Detect.json
+wget -N -qO- https://raw.githubusercontent.com/serbanghita/Mobile-Detect/master/Mobile_Detect.json | python -m json.tool > Mobile_Detect.json
