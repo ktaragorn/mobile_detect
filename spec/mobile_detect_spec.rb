4c4d992dc96f3f22856dd513cec56bfde33e523c
Sebastian Wallin <sebastian.wallin@gmail.com>
2016-03-05 17:45:18 -0800

    Use hash rocket syntax in test to handle Ruby 1.9

--- spec/mobile_detect_spec.rb
+++ spec/mobile_detect_spec.rb
@@ -91,10 +91,10 @@ describe MobileDetect do
     context 'when headers are symbols' do
       let(:test_headers) do
         {
-          'HTTP_USER_AGENT': 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0_1 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A523 Safari/8536.25',
-          'HTTP_ACCEPT': 'text/vnd.wap.wml, application/json, text/javascript, */*; q=0.01',
-          'HTTP_ACCEPT_LANGUAGE': 'en-us,en;q=0.5',
-          'HTTP_ACCEPT_ENCODING': 'gzip, deflate'
+          :HTTP_USER_AGENT => 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0_1 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A523 Safari/8536.25',
+          :HTTP_ACCEPT => 'text/vnd.wap.wml, application/json, text/javascript, */*; q=0.01',
+          :HTTP_ACCEPT_LANGUAGE => 'en-us,en;q=0.5',
+          :HTTP_ACCEPT_ENCODING => 'gzip, deflate'
         }
       end
 
